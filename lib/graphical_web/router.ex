cdc72d182446a79e7f9b8f625b95749639a14625
Tensor-Programming <abeltensor@tensor-programming.com>
2018-01-10 22:05:58 -0500

    initial commit

---
+++ lib/graphical_web/router.ex
@@ -0,0 +1,21 @@
+defmodule GraphicalWeb.Router do
+  use GraphicalWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", GraphicalWeb do
+    pipe_through :api
+    resources "/users", UserController, except: [:new, :edit]
+    resources "/posts", PostController, except: [:new, :edit]
+  end
+
+
+  forward "/api", Absinthe.Plug,
+    schema: GraphicalWeb.Schema
+
+
+  forward "/graphiql", Absinthe.Plug.GraphiQL,
+    schema: GraphicalWeb.Schema
+end
